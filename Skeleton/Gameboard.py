0e75e39eb7c2994d0bb1b209378045b5655422af
dl3447 <91109919+dl3447@users.noreply.github.com>
2021-09-30 09:52:07 +0800

    Update Gameboard.py

--- Skeleton/Gameboard.py
+++ Skeleton/Gameboard.py
@@ -1,4 +1,4 @@
-import db
+
 
 class Gameboard():
     def __init__(self):
@@ -17,9 +17,12 @@ class Gameboard():
         column -= 1
         if str(player) not in self.current_turn:
             return (False, 'It is not your turn.')
-        for i in range(0, len(self.board)):
+        if column < 0 or column >= len(self.board[0]):
+            return (False, 'Invalid column.')
+        for i in range(len(self.board) - 1, -1, -1):
             if self.board[i][column] == 0:
-                self.board[i][column] = self.player1 if player == 1 else self.player2
+                self.board[i][column] = self.player1 \
+                    if player == 1 else self.player2
                 self.current_turn = 'p2' if self.current_turn == 'p1' else 'p1'
                 self.check_winner()
                 self.remaining_moves -= 1
@@ -34,30 +37,34 @@ class Gameboard():
             for j in range(len(self.board[i])):
                 if self.board[i][j] == 0:
                     continue
-                if all(i + k < len(self.board) 
-                       and j + k < len(self.board[i]) 
+                if all(i + k < len(self.board)
+                       and j + k < len(self.board[i])
                        and self.board[i + k][j + k] == self.board[i][j]
-                       for k in range(4)) \
-                       or \
-                    all(i + k < len(self.board) 
+                       for k in range(4)):
+                    self.game_result = 'p1' \
+                        if self.board[i][j] == self.player1 else 'p2'
+                    return
+                if all(i + k < len(self.board)
                        and j - k >= 0
                        and self.board[i + k][j - k] == self.board[i][j]
-                       for k in range(4)) \
-                       or \
-                    all(j + k < len(self.board[i]) 
+                       for k in range(4)):
+                    self.game_result = 'p1' \
+                        if self.board[i][j] == self.player1 else 'p2'
+                    return
+                if all(j + k < len(self.board[i])
                        and self.board[i][j + k] == self.board[i][j]
-                       for k in range(4)) \
-                       or \
-                    all(i + k < len(self.board) 
+                       for k in range(4)):
+                    self.game_result = 'p1' \
+                        if self.board[i][j] == self.player1 else 'p2'
+                    return
+                if all(i + k < len(self.board)
                        and self.board[i + k][j] == self.board[i][j]
                        for k in range(4)):
-                    self.game_result = 'p1' if self.board[i][j] == self.player1 else 'p2'
+                    self.game_result = 'p1' \
+                        if self.board[i][j] == self.player1 else 'p2'
                     return
 
 
 '''
 Add Helper functions as needed to handle moves and update board and turns
 '''
-
-
-    
